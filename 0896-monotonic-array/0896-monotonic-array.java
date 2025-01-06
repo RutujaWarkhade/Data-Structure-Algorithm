9498bfeb1c13dbd03e9b79fa3c23007c399e9926
Rutuja Shivaji Warkhade <167538733+RutujaWarkhade@users.noreply.github.com>
2025-01-06 12:36:41 +0530

    Time: 3 ms (44.95%), Space: 59.4 MB (45.75%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static boolean isMonotonic(int[] nums) {
+        boolean increment = true;
+        boolean decrement = true;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                increment = false;
+            }
+            if(nums[i]>nums[i+1])
+            {
+                decrement = false;
+            }
+        }
+        return increment||decrement;
+
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i] = sc.nextInt();
+        }
+        boolean result = isMonotonic(nums);
+        System.out.print(result);
+    }
+}
