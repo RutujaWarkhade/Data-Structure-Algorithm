2f5a418d66470f968baf7b35f69229a444241c62
Rutuja Shivaji Warkhade <167538733+RutujaWarkhade@users.noreply.github.com>
2025-01-07 23:04:04 +0530

    Time: 1 ms (96.6%), Space: 45.8 MB (6.87%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Solution {
+    public static int[] getConcatenation(int[] nums) {
+        int[] newarray = new int[nums.length+nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            newarray[i]=nums[i];
+            newarray[i+nums.length]=nums[i];
+        }
+        
+        return newarray;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i] = sc.nextInt();
+        }
+        int[] result = Solution.getConcatenation(nums);
+        for (int i = 0; i < result.length; i++) {
+         System.out.print(result[i] + " ");
+         }
+ }
+}
