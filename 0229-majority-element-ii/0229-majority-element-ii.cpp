16baf358faa7ad2d899abbc46efb517fc49a427e
Rutuja Shivaji Warkhade <167538733+RutujaWarkhade@users.noreply.github.com>
2025-07-19 21:16:30 +0530

    Time: 0 ms (100%), Space: 23.5 MB (93.81%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
         int count1 = 0;
         int count2 = 0;
-        int ele1 = INT_MIN;
+        int ele1 = 0;
         int ele2 = INT_MIN;
-        int index = 0;
+        int index = INT_MIN;
         for(int i=0;i<n;i++)
         {
-            if(count1==0 && nums[i]!=ele1)
+            if(count1==0 && nums[i]!=ele2)
             {
                 count1 = 1;
                 ele1 = nums[i];
             }
-            else if(count2==0 && nums[i]!=ele2)
+            else if(count2==0 && nums[i]!=ele1)
             {
                 count2 = 1;
                 ele2 = nums[i];
