031e15478fb551b29752f763b27a9c5e43b8d702
Rutuja Shivaji Warkhade <167538733+RutujaWarkhade@users.noreply.github.com>
2025-07-30 22:25:21 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,65 @@
 class Solution {
 public:
+    int lowerbound(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(nums[mid]==target)
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    int upperbound(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+
+            if(nums[mid]==target)
+                {
+                    ans = mid;
+                    low = mid+1;
+                }
+            else if(nums[mid]<target)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = firstPos(nums, target);
-        if (first == -1) return {-1, -1};          // not found at all
-        int last  = lastPos(nums, target);
+        int first = lowerbound(nums, target);
+        if(first==-1)
+        {
+            return {-1, -1};
+        }
+        int last = upperbound(nums, target);
         return {first, last};
-    }
-
-private:
-    int firstPos(const vector<int>& a, int x) {
-        int l = 0, r = (int)a.size() - 1, ans = -1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (a[m] >= x) r = m - 1;
-            else l = m + 1;
-            if (a[m] == x) ans = m;
-        }
-        return ans;
-    }
-
-    int lastPos(const vector<int>& a, int x) {
-        int l = 0, r = (int)a.size() - 1, ans = -1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (a[m] <= x) l = m + 1;
-            else r = m - 1;
-            if (a[m] == x) ans = m;
-        }
-        return ans;
+        
     }
 };
