4c0cdbbcfa871f2291c6c4393912150ec8c67606
Rutuja Shivaji Warkhade <167538733+RutujaWarkhade@users.noreply.github.com>
2025-09-01 22:20:52 +0530

    Time: 0 ms (100%), Space: 15.1 MB (94.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+        int low = 0, high = nums.size() - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // Check if left half is sorted
-            if (nums[left] <= nums[mid]) {
-                // Target lies within left half
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+        while(low<=high)
+        {
+            int mid = (low + high)/2;
+            //if find then return index
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            //for handling dublicates
+            if(nums[low]==nums[mid] && nums[high] == nums[mid])
+            {
+                low++;
+                high--;
+            }
+            //for left half sorted
+            else if(nums[low] <= nums[mid])
+            {
+                if(nums[low]<=target && target<=nums[mid])
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low = mid+1;
                 }
             }
-            // Otherwise, right half is sorted
-            else {
-                // Target lies within right half
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+            //else for right half
+            else
+            {
+                if(nums[mid]<=target && target<=nums[high])
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
                 }
             }
         }
-
-        return -1; // Not found
+        return -1;
     }
+    
 };
